a3c4c7b8c78a7bbf0777e105424beb042da48c87
Ismail Mannoucha <imannouc@e3r6p2.1337.ma>
2022-06-16 21:27:08 +0100

    got max_size from allocator

--- srcs/vector/vector.hpp
+++ srcs/vector/vector.hpp
@@ -8,13 +8,6 @@ namespace ft {
     template < class T, class Alloc = std::allocator<T> >
     class vector
     {
-        private:
-            T *ptr;
-            size_t size;
-            size_t capacity;
-            size_t max_size;
-
-
         public:
             typedef typename T value_type; //The first template parameter (T)	
             typedef typename Alloc allocator_type; //The second template parameter (Alloc)	defaults to: allocator<value_type>
@@ -22,19 +15,27 @@ namespace ft {
             typedef typename T& const const_reference; //allocator_type::const_reference	for the default allocator: const value_type&
             typedef typename T* pointer; //allocator_type::pointer	for the default allocator: value_type*
             typedef typename T* const const_pointer; //allocator_type::const_pointer	for the default allocator: const value_type*
+            typedef size_t size_type; //an unsigned integral type that can represent any non-negative value of difference_type	usually the same as size_t
             // typedef iterator; //a random access iterator to value_type	convertible to const_iterator
             // typedef const_iterator; //	a random access iterator to const value_type	
             // typedef reverse_iterator; //	reverse_iterator<iterator>	
             // typedef const_reverse_iterator; //	reverse_iterator<const_iterator>	
             // typedef difference_type; //a signed integral type, identical to: iterator_traits<iterator>::difference_type	usually the same as ptrdiff_t
-            typedef size_t size_type; //an unsigned integral type that can represent any non-negative value of difference_type	usually the same as size_t
 
+        private:
+            T *ptr;
+            size_type size;
+            size_type capacity;
+            size_type max_size;
+            allocator_type alloc;
+
+        public:
             explicit vector(const allocator_type& alloc = allocator_type())
             {
                 ptr = 0;
                 size = 0;
                 capacity = 0;
-                // max_size = ;
+                max_size = alloc.max_size();
             };
 
             explicit vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
