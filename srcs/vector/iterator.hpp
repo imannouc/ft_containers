74e29e0153aefeedfb7b4156a58fad7d4ef59ad8
imannouc <ismailmannoucha1.0@gmail.com>
2022-06-27 14:28:24 +0000

    MyIterator class, did Constructors and operator= , relational operators overloads, base(), dereference operator* , wrote down some other function declarations that i'll implement next

--- srcs/vector/iterator.hpp
+++ srcs/vector/iterator.hpp
@@ -51,22 +51,105 @@ class iterator_traits<const T*>
 		typedef const T& reference;
 		typedef std::random_access_iterator_tag iterator_category;
 };
+// template <class Iterator> class reverse_iterator;
 
-template <class T, class Iter>
+
+
+template <class T>// ft::vector<T>::iterator
 class MyIterator : std::iterator<std::random_access_iterator_tag,T>
 {
-	typedef typename iterator_traits<Iterator>::value_type value_type;
-	typedef typename iterator_traits<Iterator>::difference_type difference_type;
-	typedef typename iterator_traits<Iterator>::pointer pointer;
-	typedef typename iterator_traits<Iterator>::reference reference;
+	typedef T												iterator_type;//Iterator's type
+	typedef typename iterator_traits<T>::iterator_category	iterator_category;
+	typedef typename iterator_traits<T>::value_type 		value_type;
+	typedef typename iterator_traits<T>::difference_type	difference_type;
+	typedef typename iterator_traits<T>::pointer			pointer;
+	typedef typename iterator_traits<T>::reference			reference;
 
 private:
-	pointer p;
+	iterator_type _p;
 
 public:
-	MyIterator();
+	
+	/* Member functions */
+	MyIterator() : _p(0) {};
+	MyIterator(iterator_type it)
+	{
+		this->_p = it.base();
+	};
+
+	template <class Iter>
+	MyIterator( const MyIterator<Iter> & it)
+	{
+		*this = it;
+	};
+
+	template <class Iter>
+	MyIterator& operator= (const MyIterator<Iter>& it)
+	{
+		this->_p = it.base();
+		return *this;
+	};
+
+	iterator_type base() const
+	{
+		return _p;
+	};
 	~MyIterator();
+
+
+	reference operator*() const
+	{
+		return *this->_p;
+	};
+	MyIterator operator+ (difference_type n) const;
+	MyIterator& operator++();
+	MyIterator  operator++(int);
+	MyIterator& operator+= (difference_type n);
+	MyIterator operator- (difference_type n) const;
+	MyIterator& operator--();
+	MyIterator  operator--(int);
+	MyIterator& operator-= (difference_type n);
+	pointer operator->() const;
+	reference operator[] (difference_type n) const;
+
+
+
+
+
 };
+
+	/* Non-member function overloads */
+	// relational ops , + , -	
+	template <class Iterator>
+	bool operator== (const MyIterator<Iterator>& lhs, const MyIterator<Iterator>& rhs)
+	{
+		return lhs == rhs;
+	}	
+	template <class Iterator>
+	bool operator!= (const MyIterator<Iterator>& lhs, const MyIterator<Iterator>& rhs)
+	{
+		return !(lhs == rhs)
+	}	
+	template <class Iterator>
+	bool operator<  (const MyIterator<Iterator>& lhs, const MyIterator<Iterator>& rhs)
+	{
+		return (lhs < rhs)
+	}	
+	template <class Iterator>
+	bool operator<= (const MyIterator<Iterator>& lhs, const MyIterator<Iterator>& rhs)
+	{
+		return !(lhs > rhs)
+	}	
+	template <class Iterator>
+	bool operator>  (const MyIterator<Iterator>& lhs, const MyIterator<Iterator>& rhs)
+	{
+		return (lhs > rhs)
+	}	
+	template <class Iterator>
+	bool operator>= (const MyIterator<Iterator>& lhs, const MyIterator<Iterator>& rhs)
+	{
+		return !(lhs < ths)
+	}
 
 
 #endif
