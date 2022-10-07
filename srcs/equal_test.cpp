97fdc01ad40126fb0952529ccef3805b991a8be7
imannouc <ismailmannoucha1.0@gmail.com>
2022-10-07 01:01:22 +0000

    lexicographical_compare done

--- srcs/equal_test.cpp
+++ srcs/equal_test.cpp
@@ -1,11 +1,18 @@
 #include <iostream>     // std::cout
 #include <algorithm>    // std::equal
 #include <vector>       // std::vector
+#include <cctype>       // std::tolower
 #include "vector.hpp"
 #include "equal.hpp"
 
 bool mypredicate (int i, int j) {
   return (i==j);
+}
+
+// a case-insensitive comparison function:
+bool mycomp (char c1, char c2)
+{
+  return std::tolower(c1)<std::tolower(c2);
 }
 
 int main () {
@@ -27,5 +34,21 @@ int main () {
   else
     std::cout << "The contents of both sequences differ.\n";
 
+
+
+    char foo[]="Apple";
+    char bar[]="apartment";
+
+    std::cout << std::boolalpha;
+
+    std::cout << "Comparing foo and bar lexicographically (foo<bar):\n";
+
+    std::cout << "Using default comparison (operator<): ";
+    std::cout << ft::lexicographical_compare(foo,foo+5,bar,bar+9);
+    std::cout << '\n';
+
+    std::cout << "Using mycomp as comparison object: ";
+    std::cout << ft::lexicographical_compare(foo,foo+5,bar,bar+9,mycomp);
+    std::cout << '\n';
   return 0;
 }
